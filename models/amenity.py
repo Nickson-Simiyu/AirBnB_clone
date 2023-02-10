1746b887362844f19ff17a025d1dade0eee27f8c
Nickson-Simiyu <simiyunickson1@gmail.com>
2023-02-10 01:49:54 -0800

    made executable

---
+++ models/amenity.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Class Amenity, inherits from BaseModel
+"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """
+    Represents the Amenity class and inherits from BaseModel.
+    Public class attribute:
+        name: empty string
+    """
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the Amenity class with the BaseModel's init method."""
+        super().__init__(*args, **kwargs)
+
